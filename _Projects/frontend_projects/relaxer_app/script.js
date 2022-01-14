b279dcbc1c9d612bc8ea4b84c63b38d5c1ac5859
vincepogz <85046947+vincepogz@users.noreply.github.com>
2022-01-13 20:05:02 -0500

    .

--- _Projects/frontend_projects/relaxer_app/script.js
+++ _Projects/frontend_projects/relaxer_app/script.js
@@ -1,14 +1,24 @@
-const container = document.getElementById('container');
-const text = document.getElementById('text');
+//Creating variables in javascript (Constant Referrence)
 
+//Variable container is referring to div with id='container'
+const container = document.getElementById('container');
+//Variable text is referring to div with id='text'
+const text = document.getElementById('text');
+//Variable totalTime is set to 7500
 const totalTime = 7500;
+//Variable breatheTime is set to the formula below
 const breatheTime = (totalTime / 5) * 2;
+//Variable holdTime is set to the formula below
 const holdTime = totalTime / 5;
 
+//Initialize breathAnimation Function/Method
 breathAnimation();
 
+
 function breathAnimation(){
+    //When condition is met, JS will inject the following text
     text.innerHTML = 'Breath In!';
+    //When condition is met, JS will inject the className
     container.className = 'container grow';
 
     setTimeout(() => {
